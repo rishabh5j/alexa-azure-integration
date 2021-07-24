c865167dda3418a8587ee51b37818158b7207c23
Rishabh Jain <rijain@microsoft.com>
2021-07-25 00:19:50 +0530

    changes

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import sqlite3, hashlib, os
+from werkzeug.utils import secure_filename
+import random
+import base64
+import string
+
+def randomString(stringLength=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+app = Flask(__name__)
+
+@app.route("/api")
+def root():
+    return "ok"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True)
